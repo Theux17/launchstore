31c482d2b1520f2fcbdb1a263074df650c8693a8
Theux17 <oliveira.ms18@gmail.com>
2020-06-10 23:59:55 -0300

    Criada as rotas do edit/post/put/delete

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,14 @@ routes.get("/", function(req, res){
 })
 
 routes.get("/products/create", productController.create)
+routes.get("/products/:id/edit", productController.edit)
 
+routes.post("/products", productController.post)
+routes.put("/products", productController.put)
+routes.delete("/products", productController.delete)
+
+
+// Alias (atalhos)
 routes.get("/ads/create", function(req, res){
     return res.rendirect("/products/create")
 })
