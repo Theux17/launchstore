f89c165bc4d00adcf7930b1afc72819be8642092
Theux17 <oliveira.ms18@gmail.com>
2020-06-20 16:45:54 -0300

    Adicionado script para fazer o upload das imagens

--- public/scripts.js
+++ public/scripts.js
@@ -39,8 +39,6 @@ const Mask = {
     }
 
 }
-console.log(Mask.percentage(value))
-
 /*
 
 //Desafio - 06 (Funções assíncronas)
@@ -73,3 +71,116 @@ async function doubleResultAndSum() {
 
 doubleResultAndSum()
 */
+
+const PhotosUpload = {
+    input: "",
+    preview: document.querySelector('#photos-preview'),
+    uploadLimit: 6,
+    files: [],
+    handleFileInput (event) {
+        const { files: fileList } = event.target
+        PhotosUpload.input = event.target
+
+        if(PhotosUpload.hasLimit(event)) return
+
+        Array.from(fileList).forEach(file => {
+
+            // adiciono cada file(arquivo) no array files
+            PhotosUpload.files.push(file)
+
+            const reader = new FileReader()
+
+            // Quando o reader estiver pronto, ele executa a função
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const div = PhotosUpload.getConatainer(image)
+                PhotosUpload.preview.appendChild(div)
+
+            }
+
+
+            // Esse é o momento que ele fica pronto, quando ele lê o file
+            reader.readAsDataURL(file)
+        
+        })
+
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+    },
+
+    hasLimit(event){
+        const { uploadLimit, input, preview  } = PhotosUpload
+        const { files: fileList } = input
+
+        if(fileList.length > uploadLimit){
+            alert(`Envie no máximo ${uploadLimit} fotos.`)
+            event.preventDefault()
+            return true
+        }
+
+        const photosDiv = []
+
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == "photo")
+            photosDiv.push(item)
+        })
+
+        const totalPhotos = fileList.length +   photosDiv.length
+
+        if(totalPhotos > uploadLimit ){
+            alert("Você atingiu o limite máximo de fotos!")
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+
+    getAllFiles(){
+        const dataTransfer = new DataTransfer()
+
+        // Para cada file ele vai adicionar o dataTransfer adicionado um arquivo
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+    
+        return dataTransfer.files        
+    },
+
+    getConatainer(image){
+        const div = document.createElement('div')
+        div.classList.add('photo')
+                
+        div.onclick = PhotosUpload.removePhoto
+
+        div.appendChild(image)
+
+        div.appendChild(PhotosUpload.getRemoveButton())
+
+        return div
+    },
+
+    getRemoveButton(){
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "close"
+        
+        return button
+    },
+
+    removePhoto(event){
+        const photoDiv = event.target.parentNode // <div class="photos">
+
+        // Pegando de uma lista que é o preview (dentro dele tem as fotos)
+        const photosArray = Array.from(PhotosUpload.preview)
+        const index = photosArray.indexOf(photoDiv)
+
+        // Vai remover o index que ele encontrar (que é ele mesmo), e o 2° argumento é quantos elementos do array ele vai remover (no caso abaixo é 1, que é ele mesmo)
+        PhotosUpload.files.splice(index, 1)
+        
+        // Atualizo o input com as imagens novas
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+        photoDiv.remove()
+    }
+}
