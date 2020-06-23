98e07dcc3742d3bf6f758558d7cdf2831e7eac63
Theux17 <oliveira.ms18@gmail.com>
2020-06-23 19:18:51 -0300

    Adicionada rota de visualização de produto

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,9 @@ routes.get("/", function(req, res){
 })
 
 routes.get("/products/create", productController.create)
+routes.get("/products/:id", productController.show)
+
+
 routes.get("/products/:id/edit", productController.edit)
 
 routes.post("/products", multer.array("photos", 6), productController.post)
