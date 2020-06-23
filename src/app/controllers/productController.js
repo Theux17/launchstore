1f6295ae225075ba52032800510a55b5e4a5aef0
Theux17 <oliveira.ms18@gmail.com>
2020-06-23 15:05:26 -0300

    Ajustando o put para atualizar os arquivos

--- src/app/controllers/productController.js
+++ src/app/controllers/productController.js
@@ -82,9 +82,29 @@ module.exports = {
         const keys = Object.keys(req.body)
 
         for (key of keys) {
-            if (req.body[key] == "")
+            if (req.body[key] == "" && key != "removed_files"){
                 return res.send("Please, fill all fields")
+            }
         }
+
+
+        if(req.files.length != 0){
+            const newFilesPromise = req.files.map(file => 
+                File.create({...file, product_id: req.body.id}))
+            await Promise.all(newFilesPromise)     
+        }
+
+        if(req.body.removed_files){
+            // 1,2,3
+            const removedFiles = req.body.removed_files.split(",") //[1,2,3,]
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1) // [1,2,3]
+        
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+        
+            await Promise.all(removedFilesPromise)
+        }
+
 
         req.body.price = req.body.price.replace(/\D/g, "")
 
