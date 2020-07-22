a3b89209aea52c6144ffe7981dcbf4a41da5e714
Theux17 <oliveira.ms18@gmail.com>
2020-07-22 18:40:13 -0300

    Finalizada a página de busca

--- src/routes.js
+++ src/routes.js
@@ -2,26 +2,31 @@ const express = require('express')
 const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 
-const productController = require('./app/controllers/productController')
+const ProductController = require('./app/controllers/ProductController')
 const homeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
 
+//Home
 routes.get("/", homeController.index)
 
-routes.get("/products/create", productController.create)
-routes.get("/products/:id", productController.show)
 
+// Search 
+routes.get('/products/search', SearchController.index)
 
-routes.get("/products/:id/edit", productController.edit)
+//Products
+routes.get("/products/create", ProductController.create)
+routes.get("/products/:id", ProductController.show)
+routes.get("/products/:id/edit", ProductController.edit)
 
-routes.post("/products", multer.array("photos", 6), productController.post)
-routes.put("/products", multer.array("photos", 6), productController.put)
-routes.delete("/products", productController.delete)
-
+routes.post("/products", multer.array("photos", 6), ProductController.post)
+routes.put("/products", multer.array("photos", 6), ProductController.put)
+routes.delete("/products", ProductController.delete)
 
 // Alias (atalhos)
 routes.get("/ads/create", function(req, res){
     return res.redirect("/products/create")
 })
+
 
 module.exports = routes
