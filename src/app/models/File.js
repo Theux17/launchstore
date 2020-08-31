e1ac781b40d0fae12a3d6016ffca1ac9d7e04a91
Theux17 <oliveira.ms18@gmail.com>
2020-08-31 18:16:59 -0300

    Criação/utilização da Base Model

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,39 +1,27 @@
-const db = require("../../config/db")
-const fs = require("fs")
+const Base = require('./Base')
+
+Base.init({ table: 'files' })
 
 module.exports = {
-    create({ filename, path, product_id }) {
-        const query = `
-            INSERT INTO files (
-                name,
-                path,
-                product_id
-            ) VALUES($1, $2, $3)
-            RETURNING id  
-        `
-        const values = [
-            filename,
-            path,
-            product_id
-        ]
+    ...Base,
+}
 
-        return db.query(query, values)
-    },
-    async delete(productId) {
-
-        try {
-            const result = await db.query(`SELECT * FROM files WHERE product_id = $1`, [productId])
-            const files = result.rows
-
-            files.map(file => fs.unlinkSync(file.path) )
-
-            return db.query(`
-                DELETE FROM files WHERE product_id = $1
-            
-            `, [productId])
-
-        }catch(err){
-            console.error(err)
-        }
+/*
+async delete(productId) {
+    
+    try {
+        const result = await db.query(`SELECT * FROM files WHERE product_id = $1`, [productId])
+        const files = result.rows
+        
+        files.map(file => fs.unlinkSync(file.path) )
+        
+        return db.query(`
+        DELETE FROM files WHERE product_id = $1
+        
+        `, [productId])
+        
+    }catch(err){
+        console.error(err)
     }
 }
+*/
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,106 +1,90 @@
-const { hash } = require('bcryptjs')
-const fs = require('fs')
+const Base = require('./Base')
 
-const db = require('../../config/db')
-const Product = require('../models/Product')
+Base.init({ table: 'users' })
 
 module.exports = {
-    async findOne(filters) {
-        let query = "SELECT * FROM users"
+    ...Base,
+}
 
-        // Está rodando no - WHERE | OR 
-        Object.keys(filters).map(key => {
-            query = ` ${query}
-                ${key}
+/*
+async create(data) {
+    try {
+        const query = `
+            INSERT INTO users (
+            name,
+            email,
+            password,
+            cpf_cnpj,
+            cep,
+            address
+        ) VALUES($1, $2, $3, $4, $5, $6)
+        RETURNING id
+        `
+
+        const passwordHash = await hash(data.password, 8)
+
+        const values = [
+            data.name,
+            data.email,
+            passwordHash,
+            data.cpf_cnpj.replace(/\D/g, ""),
+            data.cep.replace(/\D/g, ""),
+            data.address
+        ]
+
+        const results = await db.query(query, values)
+        return results.rows[0].id
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+},
+
+async update(id, fields) {
+    let query = "UPDATE users SET"
+
+    Object.keys(fields).map((key, index, array) => {
+        if ((index + 1) < array.length) {
+            query = `${query}
+                ${key} = '${fields[key]}',
             `
-            // Entra no where ou no or e pega o email ou cpf_cnpj            
-            Object.keys(filters[key]).map(field => {
-                query = `${query} ${field} = '${filters[key][field]}'`
-            })
-
-        })
-        const results = await db.query(query)
-        return results.rows[0]
-    },
-
-    async create(data) {
-        try {
-            const query = `
-                INSERT INTO users (
-                name,
-                email,
-                password,
-                cpf_cnpj,
-                cep,
-                address
-            ) VALUES($1, $2, $3, $4, $5, $6) 
-            RETURNING id  
+        } else {
+            query = `${query}
+                ${key} = '${fields[key]}'
+                WHERE id = ${id}
             `
-
-            const passwordHash = await hash(data.password, 8)
-
-            const values = [
-                data.name,
-                data.email,
-                passwordHash,
-                data.cpf_cnpj.replace(/\D/g, ""),
-                data.cep.replace(/\D/g, ""),
-                data.address
-            ]
-
-            const results = await db.query(query, values)
-            return results.rows[0].id
         }
-        catch (err) {
-            console.error(err);
-        }
+    })
 
-    },
+    await db.query(query)
+    return
+},
 
-    async update(id, fields) {
-        let query = "UPDATE users SET"
+async delete(id) {
+    // pegar todos os produtos
+    let results = await db.query('SELECT * FROM products WHERE user_id = $1', [id])
+    const products = results.rows
 
-        Object.keys(fields).map((key, index, array) => {
-            if ((index + 1) < array.length) {
-                query = `${query}
-                    ${key} = '${fields[key]}',
-                `
-            } else {
-                query = `${query}
-                    ${key} = '${fields[key]}'
-                    WHERE id = ${id}
-                `
+    // Dos produtos, pegar todas as imagens
+    const allFilesPromise = products.map(product =>
+        Product.files(product.id))
+
+    let promiseResults = await Promise.all(allFilesPromise)
+
+    // rodar a remmoção do usuário
+    await db.query('DELETE FROM users WHERE id = $1', [id])
+
+
+    // remover as imagens da pasta public
+    promiseResults.map(results => {
+        results.rows.map(file =>{
+            try {
+                fs.unlinkSync(file.path)
+            } catch (err) {
+                console.error(err)
             }
         })
-
-        await db.query(query)
-        return
-    },
-
-    async delete(id) {
-        // pegar todos os produtos
-        let results = await db.query('SELECT * FROM products WHERE user_id = $1', [id])
-        const products = results.rows
-
-        // Dos produtos, pegar todas as imagens 
-        const allFilesPromise = products.map(product => 
-            Product.files(product.id))
-
-        let promiseResults = await Promise.all(allFilesPromise)
-
-        // rodar a remmoção do usuário
-        await db.query('DELETE FROM users WHERE id = $1', [id])
-
-
-        // remover as imagens da pasta public
-        promiseResults.map(results => {
-            results.rows.map(file =>{
-                try {
-                    fs.unlinkSync(file.path)
-                } catch (err) {
-                    console.error(err)
-                }
-            })
-        })
-    }
+    })
 }
+*/
