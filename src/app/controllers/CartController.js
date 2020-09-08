e918c0210fd969044a3edca602c59a3837297f11
Theux17 <oliveira.ms18@gmail.com>
2020-09-08 14:36:30 -0300

    Funcionalidade de remover 1 item do carrinho

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -5,15 +5,33 @@ const LoadProducsSevices = require('../services/LoadProductServices')
 module.exports = {
     async index(req, res) {
         try {
-            const product = await LoadProducsSevices.load('product', { where: { id:  1  } } )
             let { cart } = req.session
 
             //gerenciador de carrinho
-            cart =  Cart.init(cart).addOne(product)
+            cart =  Cart.init(cart)
 
             return res.render("cart/index", { cart })
         } catch (error) {
             console.error(error)
         }
+    },
+
+    async addOne(req, res){
+        // pegar o id do produto e o produto
+        const { id } = req.params
+
+        const product = await LoadProducsSevices.load('product', { where: { id }})
+
+        // pegar o carrinho da sessão
+        let { cart } = req.session
+
+        // adicionar o produto ao carrinho (usando o gerenciador de carrinho)
+        cart = Cart.init(cart).addOne(product)
+
+        // atualizar o carrinho da sessão
+        req.session.cart = cart
+
+        // redirecionar o usuário para a tela do carrinho
+        return res.redirect('/cart')
     }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -5,5 +5,6 @@ const CartController = require('../app/controllers/CartController')
 
 // login / logout
 routes.get('/', CartController.index)
+    .post('/:id/add-one', CartController.addOne)
 
 module.exports = routes
