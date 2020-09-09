c2a060b525563b47cd6e69d28343d6d85df17206
Theux17 <oliveira.ms18@gmail.com>
2020-09-09 16:40:49 -0300

    Utilização do service e a criação do sales

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,10 +1,11 @@
 const LoadProductService = require('../services/LoadProductServices')
+const LoadOrderService = require('../services/LoadOrderService')
+
 const User = require('../models/User')
 const Order = require('../models/Order')
 
 const mailer = require('../../lib/mailer')
 const Cart = require('../../lib/cart')
-const { formatPrice, date} = require('../../lib/utils')
 
 
 const email = (seller, product, buyer) => `
@@ -28,45 +29,15 @@ const email = (seller, product, buyer) => `
 module.exports = {
     async index(req, res){
         // pegar os pedidos do usuário
-        let orders = await Order.findAll({ where: { buyer_id: req.session.userId } }) 
-    
-        const getOrdersPromise = orders.map(async order => {
-            
-            // detalhes do produto
-            order.product = await LoadProductService.load('product', { where: { id: order.product_id } })
-            
-            // detalhes do comprador
-            order.buyer = await User.findOne({ where: { id: order.buyer_id } })
-            
-            // detalhes do vendedor
-            order.seller = await User.findOne({ where: { id: order.seller_id } })
-
-            // formatação de preço
-            order.formattedPrice = formatPrice(order.price)
-            order.formattedTotal = formatPrice(order.total)
-
-            // formatação do status
-            const statuses = {
-                open: "Aberto",
-                sold: "Vendido",
-                canceled: "Cancelado"
-            }
-
-            order.formattedStatus = statuses[order.status]
-
-            // formatação de atualizado em...
-            const updatedAt = date(order.updated_at)
-            const formattedDate = `${updatedAt.day}/${updatedAt.month}/${updatedAt.year}`
-            const updateTime = `${updatedAt.hours}h${updatedAt.minutes}`
-
-            order.formattedUpdatedAt = `${order.formattedStatus} em ${formattedDate} às ${updateTime}`
-
-            return order
-        })
-
-        orders = await Promise.all(getOrdersPromise)
+        const orders = await LoadOrderService.load('orders', { where: { buyer_id: req.session.userId } })
 
         return res.render("orders/index", { orders })
+    },
+
+    async sales(req, res){
+        const sales = await LoadOrderService.load('orders', { where: { seller_id: req.session.userId } })
+
+        return res.render("orders/sales", { sales })
     },
 
     async post(req, res) {
